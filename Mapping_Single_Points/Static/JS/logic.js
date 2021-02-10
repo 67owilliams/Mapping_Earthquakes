eec6433386de8a4a3966caa249fc0db5db1251f5
67owilliams <67owilliams@gmail.com>
2021-02-09 22:23:21 -0600

    Updating repository

--- Mapping_Single_Points/Static/JS/logic.js
+++ Mapping_Single_Points/Static/JS/logic.js
@@ -25,9 +25,25 @@ console.log(map);
 // Then we add our 'graymap' tile layer to the map.
 //streets.addTo(map);
 
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+//console.log(marker)
+
+//L.circle([34.0522, -118.2437], {
+//    radius: 100
+// }).addTo(map);
+
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: "#ffffa1"
+}).addTo(map);
+
+
 // We create the tile layer that will be the background of our map.
 // Use Mapbox Styles API instead of id.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
